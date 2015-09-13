a0b90633e55cb2af56a867bd02aa9f3a3e3d8cd9
raf2987 <raf2987@yahoo.com>
2015-09-14 00:38:39 +0400

    Plotting project1

---
+++ plot1.R
@@ -0,0 +1,19 @@
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("./exdata-data-household_power_consumption.zip")){
+  download.file(fileUrl,destfile="./data/household_power_consumption.txt")
+}
+
+# Unzip the required file
+unzip(zipfile="./exdata-data-household_power_consumption.zip",exdir="./data")
+
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
